f0b95004ca8dc3a1c26c7034ab0e094b2e95351e
Nimit Patel <nimitpatel@Nimits-MacBook-Air.local>
2021-06-06 18:01:44 +0530

    prod

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,19 +15,19 @@ const callbackMiddleware = passport.authenticate('google', { session: false })
 
 const setToken = (req, res) => {
     let token = req.user.token
-    //res.cookie('ecom', token, { domain: '.ecom.me', expires: 10 });
-    //res.redirect('https://ecom.me/');
-    res.cookie('ecom', token);
-    res.redirect('http://localhost:3000/');
+    res.cookie('ecom', token, { domain: '.proffie.me', expires: 10 });
+    res.redirect('https://ecom.proffie.me/');
+    // res.cookie('ecom', token);
+    // res.redirect('http://localhost:3000/');
 }
 
 const logout = async (req, res) => {
-    //await res.clearCookie('ecom', { domain: '.ecom.me' });
-    //await res.clearCookie(`ecom.sig`, { domain: '.ecom.me' });
-    //res.redirect('https://ecom.me/');
-    await res.clearCookie('ecom');
-    await res.clearCookie(`ecom.sig`);
-    res.redirect('http://localhost:3000/');
+    await res.clearCookie('ecom', { domain: '.proffie.me' });
+    await res.clearCookie(`ecom.sig`, { domain: '.proffie.me' });
+    res.redirect('https://ecom.proffie.me/');
+    // await res.clearCookie('ecom');
+    // await res.clearCookie(`ecom.sig`);
+    // res.redirect('http://localhost:3000/');
 }
 
 module.exports = {
